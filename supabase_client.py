9fe25e3d7e2973d8669eedc0b52e11799ca7507f
bschinmay2005-glitch <bschinmay2005@gmail.com>
2026-06-15 23:54:37 +0530

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,13 @@
 from supabase import create_client
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 supabase = create_client(
-    os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_KEY")
+    SUPABASE_URL,
+    SUPABASE_KEY
 )
